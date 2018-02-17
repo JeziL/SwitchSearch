7b5fcd413a8a2e81e59087917477098aec3be386
Li <me@wangjinli.com>
2018-02-18 00:18:09 +0800

    Determine current engine.

--- static/js/popover.js
+++ static/js/popover.js
@@ -1,4 +1,18 @@
 var contentBox = document.getElementById('engines');
 var $global = safari.extension.globalPage.contentWindow;
 
-contentBox.innerHTML = $global.engines[0].name;
+var engines = $global.engines;
+var hostname = $global.currentHostname();
+var currentEngine = null;
+for (var i = 0; i < engines.length; i++) {
+    var e = engines[i];
+    if (hostname.indexOf(e.hostnameKeyword) >= 0) {
+        currentEngine = e;
+    }
+}
+
+if (currentEngine == null) {
+    contentBox.innerHTML = "Unsupported website.";
+} else {
+    contentBox.innerHTML = currentEngine.name;
+}
